b68646397f6655b5e700a9ecc9c1065c1eede4cf
Henri Gros <henrigros1@gmail.com>
2026-07-31 16:17:00 +0200

    Devenv sync process

--- devenv.nix
+++ devenv.nix
@@ -19,13 +19,20 @@
     openssl
     alsa-lib
     cmake
-
-    udev
-    inotify-tools
     rsync
   ];
 
   env.LD_LIBRARY_PATH = lib.makeLibraryPath [
     pkgs.libclang
   ];
+
+  processes.sync = {
+    exec = "rsync . root@luwudynamics.home:/root/argos/ -av --delete --no-owner --no-group --no-perms --exclude='target/' --exclude='.git/' --exclude='.devenv/' --exclude='.gitignore'";
+    watch = {
+      paths = [
+        ./src
+        ./Cargo.toml
+      ];
+    };
+  };
 }
